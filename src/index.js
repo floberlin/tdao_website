ac61fc034a0bf71faf1418b28b80d80003d5f0d4
Michels <carsten.michels@t-systems.com>
2022-02-09 16:12:54 +0100

    Horizon Theme

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import "@ui5/webcomponents/dist/Assets.js";
 import "@ui5/webcomponents-fiori/dist/Assets.js";
 import "@ui5/webcomponents-react/dist/Assets.js";
 import "@ui5/webcomponents/dist/features/InputElementsFormSupport.js";
-import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
 
 import React from "react";
 import ReactDOM from "react-dom";
 
 import "./util/i18n";
 import App from "./App";
-setTheme("sap_horizon");
 ReactDOM.render(<App />, document.getElementById("root"));
